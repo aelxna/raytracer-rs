95159ec4c7eca73304ef137eadc7b363e9d5c529
aelxna <aelxna@proton.me>
2026-03-05 21:42:46 -0600

    i made cat!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    let fp = fs::read_to_string(file_path).expect("Failed to read file");
+
+    let lines: Vec<&str> = fp.split('\n').collect();
+
+    for line in lines {
+        println!("{line}");
+    }
+    // TODO: generate config from lines
+
+    // TODO: determine color values at each pixel
+
+    // TODO: write header to output file
+
+    // TODO: write pixels to output file
 }
